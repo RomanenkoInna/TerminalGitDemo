4884f3d7c3312cb28a2f27763e7701a734b78439
Inna Romanenko <tenet@MacBook-Pro-Inna.local>
2021-09-13 23:17:46 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Inna Romanenko on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
